fab18d945e0f8c7107b6e2feb9034098000c9ed7
Gustavo Oliveira <gustavo.ramosue@gmail.com>
2023-03-06 01:00:18 -0300

    Add project files.

---
+++ FibonacciMod/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int digit_u;
+        double ant = 1, prox = 0;
+
+        Console.WriteLine("Informe o número para o cálculo ");
+        digit_u = int.Parse(Console.ReadLine());
+        Console.Write($"A sequencia fibonacci de {digit_u} é: ");
+        for (int i = 0; i < digit_u; i++)
+        {
+            Console.Write(ant + " | ");
+            ant = prox + ant;
+            prox = ant - prox;
+
+        }
+    }
+}
